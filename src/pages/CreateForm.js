8d6537a1a0f3a7ffa46a8b95ee8b5cf93215df9f
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-15 21:30:06 +0530

    form sumbit, fetch, post request worked

--- src/pages/CreateForm.js
+++ src/pages/CreateForm.js
@@ -1,7 +1,51 @@
+import { useState } from "react";
+
 const CreateForm = () => {
+  //
+  //   const [formDataObject, setFormDataObject] = useState({});
+  //
+  const formHandler = async () => {
+    //
+    // console.log(formDataObject, "formDataObject-start");
+    //form html portion
+    const name = document.getElementById("name").value;
+    const description = document.getElementById("description").value;
+
+    const formObject = { name: name, description: description };
+    console.log(formObject, "formObject");
+    // setFormDataObject(formObject);
+    // console.log(formDataObject, "formDataObject-end");
+
+    try {
+      const response = await fetch("http://localhost:3006/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: name,
+          description: description,
+        }),
+      });
+      //
+      const { createdTask } = await response.json();
+      console.log(createdTask, "createdTask");
+      //
+    } catch (err) {
+      console.log(err);
+    }
+
+    //
+  };
+  //
   return (
     <div>
       <h2>Create Form</h2>
+
+      <label>Name</label>
+      <input id="name" type="text" />
+      <label>Description</label>
+      <textarea id="description" type="text" />
+
+      <button onClick={formHandler}>Submit here</button>
     </div>
   );
 };
