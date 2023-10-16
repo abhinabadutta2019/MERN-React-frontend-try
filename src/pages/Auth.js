ba772a84defef8cf4d210878f7eb63af92eebec5
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-16 05:58:11 +0530

    form function working on

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,13 +1,19 @@
 const Auth = () => {
+  //
+  const registrationHandler = (event) => {
+    event.preventDefault();
+    console.log("Hi");
+  };
+  //
   return (
     <div>
       <h2>Auth Page</h2>
-      <form>
+      <form onSubmit={registrationHandler}>
         <label>Username</label>
         <input type="text" required />
         <label>Password</label>
         <input type="text" required />
-        <button>Register</button>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
