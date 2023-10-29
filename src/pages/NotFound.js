b938c3a3735a11f3a13d442f2b7c82d8f24b5872
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-29 22:30:24 +0530

    added 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,9 +2,14 @@ import React from "react";
 
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div
+      className="container d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="text-center">
+        <h1>404 - Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+      </div>
     </div>
   );
 }
