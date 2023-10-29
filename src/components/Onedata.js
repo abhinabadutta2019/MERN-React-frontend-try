4bca8319f753eaebb2752bc942c07a7f6879cf15
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-29 18:54:04 +0530

    added at Onedata

--- src/components/Onedata.js
+++ src/components/Onedata.js
@@ -1,20 +1,21 @@
 const Onedata = ({ dataFields, statusHandler, deleteHandler }) => {
   return (
-    <>
-      <h3>{dataFields.name}</h3>
-      <p>Description : {dataFields.description}</p>
-      <p>Slot : {dataFields.slot}</p>
-      {
-        <p>
-          Work status :
-          {dataFields.completed ? <> Completed</> : <> Not completed</>}
+    <div className="card mt-3">
+      <div className="card-body">
+        <h3 className="card-title">{dataFields.name}</h3>
+        <p className="card-text">Description: {dataFields.description}</p>
+        {/* <p className="card-text">Slot: {dataFields.slot}</p> */}
+        <p className="card-text">
+          Work status: {dataFields.completed ? "Completed" : "Not completed"}
         </p>
-      }
-      <button onClick={statusHandler}>Change status</button>
-      {/*  */}
-      <br />
-      <button onClick={deleteHandler}>Delete button</button>
-    </>
+        <button className="btn btn-primary" onClick={statusHandler}>
+          Change status
+        </button>
+        <button className="btn btn-danger ms-2" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+    </div>
   );
 };
 
